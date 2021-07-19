2653e560120b77c4e15854a3a187b0e1cc0cf2bf
Eti Priya <etipriya@gmail.com>
2021-07-19 18:40:37 +0100

    changed Port in server

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const { DB_URL, MONGOOSE_OPTIONS } = require("./config/config");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -14,10 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+// app.use(express.static(path.resolve(__dirname, "public")));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
