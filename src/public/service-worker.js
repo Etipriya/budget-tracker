680aaec26a1a9e522bbaedabe4ed720fa7fae585
Eti Priya <etipriya@gmail.com>
2021-07-21 22:41:23 +0100

    modified service-worker file

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -1,13 +1,9 @@
 // Implement service worker
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
-  "/favorites.html",
-  "/topic.html",
-  "/assets/css/style.css",
-  "/dist/app.bundle.js",
-  "/dist/favorites.bundle.js",
-  "/dist/topic.bundle.js",
+  "../public/index.html",
+  "../public/styles.css",
+  "../public/index.js",
   "https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap",
   "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
 ];
