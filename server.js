0a9b51aef9520ec33c8d2bc88f268b235c5a00c7
Eti Priya <etipriya@gmail.com>
2021-07-21 23:12:04 +0100

    changed the path to routes

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-// const { DB_URL, MONGOOSE_OPTIONS } = require("./config/config");
-
 const PORT = 3000;
 
 const app = express();
@@ -23,7 +21,7 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./src/routes/api"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
