8a5110f37d27ab093f5b046fc4de30dcd999cd91
Eti Priya <etipriya@gmail.com>
2021-07-21 23:13:55 +0100

    added activate events to remove old cache

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -22,3 +22,25 @@ self.addEventListener("install", event => {
       .then(self.skipWaiting())
   );
 });
+
+//Adding activate events to remove old cache
+self.addEventListener("activate", event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        return cacheNames.filter(
+          cacheName => !currentCaches.includes(cacheName)
+        );
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
