2aaa7919359a2bdcb59061ad48ec774056bb2029
Eti Priya <etipriya@gmail.com>
2021-07-22 16:07:11 +0100

    modified implement service worker

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -1,46 +1,19 @@
 // Implement service worker
-const FILES_TO_CACHE = [
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+var urlsToCache = [
   "/",
-  "../public/index.html",
-  "../public/styles.css",
-  "../public/index.js",
-  "../public/icons/icon-192x192.png",
-  "../public/icons/icon-512x512.png",
-  "https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap",
-  "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
-// Adding event listener to install PRECACHE event
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
-  );
-});
+// Adding event listener to install  event
 
 //Adding activate events to remove old cache
-self.addEventListener("activate", event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
-        );
-      })
-      .then(cachesToDelete => {
-        return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
-  );
-});
+
+//Added event listener to fetch
