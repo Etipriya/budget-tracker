2ebde53b2e13f100a78deadd39bb3d8c1116d349
Eti Priya <etipriya@gmail.com>
2021-07-23 23:09:28 +0100

    added event listener to fetch and remove old cache

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -23,6 +23,45 @@ self.addEventListener("install", function (event) {
   );
 });
 
-//Adding activate events to remove old cache
-
 //Added event listener to fetch
+self.addEventListener("fetch", function (event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        })
+        .catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  //Adding activate events to remove old cache
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
